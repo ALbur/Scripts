bc35bd4411ed0fd411af75d8c807d86392920212
ALbur <24468956+ALbur@users.noreply.github.com>
2022-10-31 23:33:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+echo 'src-git nas_istore https://github.com/istoreos/istoreos.git;main' >> feeds.conf.default
